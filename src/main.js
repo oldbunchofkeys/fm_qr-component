ddd1c247dd6f951a86d051d946dcf9d6f5159ef0
Pete Thompson <peter.thompson@roswellstudios.com>
2022-11-18 19:04:51 -0500

    fix: set lang attribute value for a11y

--- src/main.js
+++ src/main.js
@@ -5,4 +5,8 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  created() {
+    const html = document.documentElement // returns the html tag
+    html.setAttribute('lang', 'en')
+  }
 }).$mount('#app')
